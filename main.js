c6161d2bb1181825100d3dcb9ad00c8cd4a032cd
vrushit <bharuchavrushit@ymail.com>
2020-08-30 19:16:37 +0530

    Development phases

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isWindow = process.platform === "win32" ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -7,7 +12,8 @@ function createMainWindow() {
     title: "Image Resizer",
     width: 500,
     height: 600,
-    icon: "./assets/Icon_256x256.png",
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
   });
 
   //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -16,3 +22,5 @@ function createMainWindow() {
 }
 
 app.on("ready", createMainWindow);
+
+app.allowRendererProcessReuse = true;
