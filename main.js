6b82b123b0425cde5e3747f4feba3cf1c2f33338
vrushit <bharuchavrushit@ymail.com>
2020-08-30 19:05:13 +0530

    Adding icon to the taskbar

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createMainWindow() {
     title: "Image Resizer",
     width: 500,
     height: 600,
+    icon: "./assets/Icon_256x256.png",
   });
 
   //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
