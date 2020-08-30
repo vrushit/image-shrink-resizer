9c59cd7a3e046003a961d39b93f04ebabe61de4f
vrushit <bharuchavrushit@ymail.com>
2020-08-30 18:29:13 +0530

    Window

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello World");
+const { app, BrowserWindow } = require("electron");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 500,
+    height: 600,
+  });
+}
+
+app.on("ready", createMainWindow);
